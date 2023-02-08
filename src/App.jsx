bbad2337772b6baa7e0a3f6a1da83cae5793fa0c
szymon <skimpywhell6546@gmail.com>
2023-02-08 20:03:36 +0100

    icons/github

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import '@fontsource/roboto/400.css';
-import { Typography } from '@mui/material';
+import { Typography, BottomNavigation, BottomNavigationAction } from '@mui/material';
 import Chart from './components/Chart';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
     <div>
       <Typography variant="h3" component="h1">Sortowanie</Typography>
       <Chart />
+      <BottomNavigation>
+        <a href="https://github.com/sws6546/ReactSort/tree/master/src"><BottomNavigationAction label="Kod" icon={<GitHubIcon />}/></a>
+      </BottomNavigation>
     </div>
   )
 }
