36379ee03d68936a87dbcb722b9b3ac741c1ea0b
szymon <skimpywhell6546@gmail.com>
2023-02-09 18:04:14 +0100

    sortowanieBySelection

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
+import {TextField, Button, Select, MenuItem, InputLabel, FormControl} from '@mui/material';
 import './Chart.css';
-import {TextField, Button} from '@mui/material';
 
 const Chart = () => {
     const [list, setList] = useState([]);
     const [speed, setSpeed] = useState(100);
+    const [algorithm, setAlgorithm] = useState("bubble");
 
     useEffect(() => {
         let lista = []
@@ -28,18 +29,37 @@ const Chart = () => {
         let listElement = list.length
         let tempList = [...list]
 
-        for (let i = 0; i < listElement-1; i++) {
-            for (let j = 0; j < listElement-1; j++) {
-                if(tempList[j] > tempList[j+1]) {
-                    let temp = tempList[j]
-                    tempList[j] = tempList[j+1]
-                    tempList[j+1] = temp
-                    await new Promise(r => setTimeout(r, speed));
-                    setList([...tempList])
-                    console.log(tempList)
+        if(algorithm === "bubble") {
+            for (let i = 0; i < listElement-1; i++) {
+                for (let j = 0; j < listElement-1; j++) {
+                    if(tempList[j] > tempList[j+1]) {
+                        let temp = tempList[j]
+                        tempList[j] = tempList[j+1]
+                        tempList[j+1] = temp
+                        await new Promise(r => setTimeout(r, speed));
+                        setList([...tempList])
+                    }
                 }
             }
         }
+        else if(algorithm === "bySelection") {
+            for(let i = 0; i < listElement - 1; i++){
+                let min = tempList[i]
+                let minIdx = i
+                for(let j = i; j < listElement ; j++){
+                    if(tempList[j] < min){
+                        min = tempList[j]
+                        minIdx = j
+                    }
+                }
+                let temp = tempList[i]
+                tempList[i] = min
+                tempList[minIdx] = temp
+                await new Promise(r => setTimeout(r, speed));
+                setList([...tempList])
+            }
+        }
+
         document.getElementById('sortClick').disabled = false
     }
 
@@ -58,12 +78,31 @@ const Chart = () => {
             </div>
             <div className="Chart">
                 {
-                    list.map((item) => {
-                        return <div className='Chart_item' style={{ height: `${item}%`}}></div>
+                    list.map((item, index) => {
+                        return <div key={index} className='Chart_item' style={{ height: `${item}%`}}></div>
                     })
                 }
             </div>
-            <Button id="sortClick" variant="contained" size="large" onClick={Sort}>Układaj</Button>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "15px"
+            }}>
+                <FormControl sx={{minWidth: 250}}>
+                    <InputLabel id="wyborAlgorytmuLabel">Wybór algorytmu</InputLabel>
+                    <Select
+                        labelId="wyborAlgorytmuLabel"
+                        label="Wybór algorytmu"
+                        id="wyborAlgorytmu"
+                        onChange={(e) => setAlgorithm(e.target.value)}
+                        defaultValue={"bubble"}
+                    >
+                        <MenuItem value={"bubble"}>Bąbelkowe</MenuItem>
+                        <MenuItem value={"bySelection"}>Przez wybór</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button id="sortClick" variant="contained" size="large" onClick={Sort}>Układaj</Button>
+            </div>
         </div>
     );
 }
